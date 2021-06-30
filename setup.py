9edeb7d06519c344d73e9b9fd0953da7b70479e4
ggrieco-tob <gustavo.grieco@trailofbits.com>
2021-06-30 14:18:09 +0200

    missing file

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="echidna-parade",
+    version="0.1",
+    description="Meta-tool to test a contract with various configs, using Echidna processes",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    packages=[
+        "echidna_parade",
+    ],
+    license="AGPL3",
+    entry_points="""
+    [console_scripts]
+    echidna-parade = echidna_parade.__main__:main
+    """,
+    keywords="echidna smart-contracts testing fuzzing swarm test-diversity",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=["pyyaml", "slither-analyzer", "crytic-compile"],
+    url="https://github.com/crytic/echidna-parade",
+)
